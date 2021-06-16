514c44ffe7b9dfabf31be1a67f5553b829ba6db4
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-06-16 11:21:29 +0530

    socket io set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import io from "socket.io-client";
 import './App.css';
 
 function App() {
+  const socket = io('http://localhost:5000');
+  const [message, setMessage] = useState('');
+  socket.on('message', (data)=>{
+    setMessage(data);
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Frontend</h1>
+      <h2>{message}</h2>
     </div>
   );
 }
