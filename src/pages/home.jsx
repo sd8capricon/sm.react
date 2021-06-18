21f3343a4385d7c3fc69a1a43d9947d02184268c
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-06-18 23:01:07 +0530

    tracks active user but the rerender issue is not solved it persists

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useContext } from 'react';
 import { SocketContext } from '../contexts/SocketContext';
 import useFriendList from '../hooks/useFriendList';
@@ -8,23 +9,33 @@ export default function Home(){
     //IMP fix component rerenders thrice
     
     const socket = useContext(SocketContext);
-    const token = localStorage.getItem('userToken');
-    const username = localStorage.getItem('username');    
+    const username = localStorage.getItem('username');
+    const token = localStorage.getItem('userToken');    
     const [friends, getFriendError] = useFriendList(username);
     const [verified, verificationError] = useUserStatus(token);
-    console.log(verified);
+    const [activeUsers, setActiveUsers] = useState([]);
     if(verified){
         console.log("connected");
         socket.auth = { username };
         socket.connect();
     }
-    console.log(friends);
+
+    socket.on("users", (users)=>{
+        console.log(users.length);
+        users = users.filter(user => user.username!==username);
+        setActiveUsers(users)
+    });
 
     return(
         <div>
             <h1>{verified? "hello" : "login first"}</h1>
+            <h2>Your Friends</h2>
+            {verified? <ol>
+                {friends.map((friends, index)=>{return(<li key={index}>{friends.username}</li>);})}
+            </ol>: <p></p>}
+            <h2>Your Online Friends</h2>
             <ol>
-                {friends.map((friends ,index)=>{return(<li key={index}>{friends.username}</li>);})}
+                {activeUsers.map((user, index)=>{return(<li key={index}>{user.username}</li>)})}
             </ol>
         </div>
     );
